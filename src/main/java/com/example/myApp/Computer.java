16bd47101c0f18277733cff0995b6c198e3696a0
PraneethaReddy0612 <Praneethareddy0612@gmail.com>
2025-03-20 12:16:47 -0400

    [Praneetha]:Add @primary annotation

--- src/main/java/com/example/myApp/Computer.java
+++ src/main/java/com/example/myApp/Computer.java
@@ -1,5 +1,8 @@
 package com.example.myApp;
 
+import org.springframework.context.annotation.Primary;
+
+@Primary
 public interface Computer {
      void compile();
 }
--- src/test/java/com/example/myApp/MyAppApplicationTests.java
+++ src/test/java/com/example/myApp/MyAppApplicationTests.java
@@ -1,4 +1,4 @@
-package src.test.java.com.example.myApp;
+package com.example.myApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
